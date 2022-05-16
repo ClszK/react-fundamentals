16136e0e2c6c003b98e4af1a5fa9abe4c0299dee
Clsz <45088680+JH-Clsz@users.noreply.github.com>
2022-05-17 02:59:54 +0900

    contact select

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
+import Contact from './Contact'
+
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      name:'hi'
-    };
-  }
   render(){
     return(
-      <div>
-        <button onClick={()=>{this.setState({name:'test'})}}>Click me</button>
-        <h1>Hello!!!!!! {this.state.name}</h1>
-      </div>
+      <Contact/>
     );
   }
 }
